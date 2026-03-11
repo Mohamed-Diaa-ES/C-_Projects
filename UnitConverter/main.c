e950558b6c814808ab18b703b5f52302edbb95c8
Mohamed Diaa <mohammeddiaato@gmail.com>
2026-03-11 07:49:51 +0200

    Fixing the bug of taking input even when invalid input is entered

--- UnitConverter/main.c
+++ UnitConverter/main.c
@@ -13,14 +13,12 @@ int main()
     {
         WelcomingAndQuantityAsking(&choice);
         // as the value of macros is defined to be less than choosen always by one like length is choosed at 1 but it is defined as 0
-        if ((choice - 1) == Exit)
+        if ((choice - 1) == Exit) // exit the program noramlly
         {
             clearScreen();
             ExitProgram();
             return 0;
         }
-
-        // TakingInputFromUser(&ToBeConverted);
         //  as the value of macros is defined to be less than choosen always by one like length is choosed at 1 but it is defined as 0
         ToBeConverted.MeasuredType = choice - 1;
 
@@ -28,24 +26,31 @@ int main()
         {
         case Length:
 
+            TakingInputFromUser(&ToBeConverted);
             // LengthCoversion(&ToBeConverted);
             break;
         case Mass:
+            TakingInputFromUser(&ToBeConverted);
             // MassCoversion(&ToBeConverted);
             break;
         case Volume:
+            TakingInputFromUser(&ToBeConverted);
             // VolumeCoversion(&ToBeConverted);
             break;
         case Time:
+            TakingInputFromUser(&ToBeConverted);
             // TimeCoversion(&ToBeConverted);
             break;
         case Digital_Storage:
+            TakingInputFromUser(&ToBeConverted);
             // Digital_StorageCoversion(&ToBeConverted);
             break;
         case Speed:
+            TakingInputFromUser(&ToBeConverted);
             // SpeedCoversion(&ToBeConverted);
             break;
         case Temprature:
+            TakingInputFromUser(&ToBeConverted);
             // TempratureCoversion(&ToBeConverted);
             break;
         default:
