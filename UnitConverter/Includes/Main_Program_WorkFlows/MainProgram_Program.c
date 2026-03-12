48aeaf267f0dc865b5de551bd34622f3a81e740f
Mohamed Diaa <mohammeddiaato@gmail.com>
2026-03-12 05:13:54 +0200

    MainProgram_Program changes:     adding print Unit function to be used all over the Conversion Functions     adding the arrays of symbols to be used in printing and putting constraints

--- UnitConverter/Includes/Main_Program_WorkFlows/MainProgram_Program.c
+++ UnitConverter/Includes/Main_Program_WorkFlows/MainProgram_Program.c
@@ -1,6 +1,95 @@
 #include "MainProgram_interfaces.h"
 #include <stdio.h>
 /**
+ * @defgroup    UnitsSymbols
+ * @brief       This group has the arrays of strings to hold the symbols of the available Quntities to be used for printing.
+ * @{ 
+ */
+/**
+ * @brief This array holds all the symbols available for the Length Quantities to be used in printing for convienience.
+ * @note  Ensure the Number of the symbols wanted to be shown is same as the  LengthUnitsNumbers Macro.
+ */
+const char *Length_Symbols[LengthUnitsNumbers] = {CM_Symb, Meters_Symb, KiloMeters_Symb, Miles_Symb, Inches_Symb, Foot_Symb, Yard_Symb};
+/**
+ * @brief This array holds all the symbols available for the Mass Quantities to be used in printing for convienience.
+ * @note  Ensure the Number of the symbols wanted to be shown is same as the  MassUnitsNumbers Macro.
+ */
+const char *Mass_Symbols[MassUnitsNumbers] = {Grams_Symb, KiloGrams_Symb, Tons_Symb, Ounces_Symb, Pound_Symb, Stone_Symb};
+/**
+ * @brief This array holds all the symbols available for the Volume Quantities to be used in printing for convienience.
+ * @note  Ensure the Number of the symbols wanted to be shown is same as the  VolumeUnitsNumbers Macro.
+ */
+const char *Volume_Symbols[VolumeUnitsNumbers] = {MilliLiter_Symb, Liter_Symb, CubicMeter_Symb, Gallon_Symb};
+/**
+ * @brief This array holds all the symbols available for the Time Quantities to be used in printing for convienience.
+ * @note  Ensure the Number of the symbols wanted to be shown is same as the  TimeUnitsNumbers Macro.
+ */
+const char *Time_Symbols[TimeUnitsNumbers] = {Millisecond_Symb, Second_Symb, Minute_Symb, Hour_Symb, Day_Symb, Week_Symb};
+/**
+ * @brief This array holds all the symbols available for the Digital_Storage Quantities to be used in printing for convienience.
+ * @note  Ensure the Number of the symbols wanted to be shown is same as the  Digital_StorageUnitsNumbers Macro.
+ */
+const char *Digital_Storage_Symbols[Digital_StoragUnitsNumbers] = {Bit_Symb, Byte_Symb, Kilobyte_Symb, Megabyte_Symb, Gigabyte_Symb, Terabyte_Symb};
+/**
+ * @brief This array holds all the symbols available for the Speed Quantities to be used in printing for convienience.
+ * @note  Ensure the Number of the symbols wanted to be shown is same as the  SpeedUnitsNumbers Macro.
+ */
+const char *Speed_Symbols[SpeedUnitsNumbers] = {MeterPerSec_Symb, KMPerHr_Symb, Milesperhour_Symb};
+/**
+ * @brief This array holds all the symbols available for the Temprature Quantities to be used in printing for convienience.
+ * @note  Ensure the Number of the symbols wanted to be shown is same as the  TempratureUnitsNumbers Macro.
+ */
+const char *Temprature_Symbols[TempratureUnitsNumbers] = {Fahrenheit_Symb, Celsius_Symb, Kelvin_Symb};
+/**
+ * @brief This array holds all the available units numbers of different Quantities.
+ * @note  Ensure the Number of the Macros wanted to be used as constraints is same as the  ConvertableQuantitesNumber Macro.
+ */
+const uint8_t Max_ValueForQuantitySymbol[ConvertableQuantitesNumber] = {LengthUnitsNumbers, MassUnitsNumbers, VolumeUnitsNumbers, TimeUnitsNumbers,
+                                                                        Digital_StoragUnitsNumbers, SpeedUnitsNumbers, TempratureUnitsNumbers};
+/**
+ * @}
+ */
+void Print_Unit(Unit *ToBePrinted)
+{
+        switch (ToBePrinted->MeasuredType)
+        {
+        case Length:
+                printf("Measured Quantity: Length, Value= %lf in ", ToBePrinted->Value);
+                printf("%s\n", Length_Symbols[ToBePrinted->UnitType]);
+                break;
+        case Mass:
+                printf("Measured Quantity: Mass, Value= %lf in ", ToBePrinted->Value);
+                printf("%s\n", Mass_Symbols[ToBePrinted->UnitType]);
+                break;
+        case Volume:
+                printf("Measured Quantity: Volume, Value= %lf in ", ToBePrinted->Value);
+                printf("%s\n", Volume_Symbols[ToBePrinted->UnitType]);
+                break;
+        case Time:
+                printf("Measured Quantity: Time, Value= %lf in ", ToBePrinted->Value);
+                printf("%s\n", Time_Symbols[ToBePrinted->UnitType]);
+                break;
+        case Digital_Storage:
+                printf("Measured Quantity: Digital_Storage, Value= %lf in ", ToBePrinted->Value);
+                printf("%s\n", Digital_Storage_Symbols[ToBePrinted->UnitType]);
+                break;
+        case Speed:
+                printf("Measured Quantity: Speed, Value= %lf in ", ToBePrinted->Value);
+                printf("%s\n", Speed_Symbols[ToBePrinted->UnitType]);
+                break;
+        case Temprature:
+                printf("Measured Quantity: Temprature, Value= %lf in ", ToBePrinted->Value);
+                printf("%s\n", Temprature_Symbols[ToBePrinted->UnitType]);
+                break;
+        default:
+                // clearScreen();
+                printf("Invalid Type!! How did you even reach this far!!! \n");
+                return;
+                break;
+        }
+}
+
+/**
  * @fn    ChooseUnitConverted
  * @brief HelperFunction to the Function @fn TakingInputFromUser
  */
